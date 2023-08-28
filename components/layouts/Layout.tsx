08eaa34a3dfa126f82abee053a8b443f94843f15
jmrk02 <jeanmarcoe2@gmail.com>
2023-08-28 10:08:51 -0500

    first commit

---
+++ components/layouts/Layout.tsx
@@ -0,0 +1,35 @@
+import { FC } from "react";
+
+import Head from "next/head";
+import { Navbar } from "../ui";
+
+interface Props {
+  title?: string;
+  children: React.ReactNode;
+}
+
+export const Layout: FC<Props> = ({ children, title }) => {
+  return (
+    <>
+      <Head>
+        <title>{title || "PokemonApp"}</title>
+        <meta name="author" content="Fernando Herrera" />
+        <meta
+          name="description"
+          content={`Información sobre el pokémon ${title}`}
+        />
+        <meta name="keywords" content={`${title}, pokemon, pokedex`} />
+      </Head>
+
+      <Navbar />
+
+      <main
+        style={{
+          padding: "0px 20px",
+        }}
+      >
+        {children}
+      </main>
+    </>
+  );
+};
